a38cc182f2b4cb55ac9c3aabea4de80101794a5c
Callum <39635013+Zelatrix@users.noreply.github.com>
2019-03-01 21:58:42 +0000

    Add files via upload

---
+++ Ackermann.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE BangPatterns #-}
+ackermann :: Int -> Int -> Int
+
+ackermann !m !n
+    | (m == 0) = n + 1
+    | (m > 0) && (n == 0) = ackermann(m-1) 1
+    | (m > 0) && ( n > 0) = ackermann(m-1) (ackermann m (n-1))
